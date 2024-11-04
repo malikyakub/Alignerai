123589cc85aed910cd7051efc7947c2ee22ec530
malik <malikyakub@hotmail.com>
2024-11-04 17:15:11 +0300

    from tsx to jsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,22 +1,14 @@
 import React from "react";
 import Header from "./components/Intro";
 import Footer from "./components/Footer";
-import { useState } from "react";
 
 function App() {
-  const [page, setPage] = useState("page-1");
-  if (page == "page-1") {
-    return (
-      <div className="flex flex-col h-[100vh] items-center justify-around">
-        <Header />
-        <Footer />
-      </div>
-    );
-  } else if (page == "page-2") {
-    return <h1>page two </h1>;
-  } else if (page == "page-3") {
-    return <h1>page three</h1>;
-  }
+return (
+  <div className="flex flex-col h-[100vh] items-center justify-around">
+    <Header />
+    <Footer />
+  </div>
+);
 }
 
 export default App;
